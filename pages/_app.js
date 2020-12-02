acaea93824480cfcd9005f91a8d567cf5d6c64e9
Doug Richar <drichar@gmail.com>
2020-12-02 13:02:15 -0500

    Update meta viewport tag

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <Component {...pageProps} />
     </>
