136528bed89a5d588339545805141bd158ae5aff
Doug Richar <drichar@gmail.com>
2020-12-02 20:12:31 -0500

    Pink icons for active mobile menu items

--- components/Menu.js
+++ components/Menu.js
@@ -9,12 +9,12 @@ export default function Menu() {
 
   const className = {
     default: {
-      inactive: 'px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-white-5',
-      active: 'px-3 py-2 rounded-md text-base font-medium text-white bg-black-50'
+      inactive: 'px-3 py-2 rounded-md menu-inactive',
+      active: 'px-3 py-2 rounded-md menu-active'
     },
     mobile: {
-      inactive: 'group flex items-center px-3 py-3 text-base font-medium text-gray-300 rounded-md hover:text-white hover:bg-white-5',
-      active: 'group flex items-center px-3 py-3 text-base font-medium text-white rounded-md bg-black-50'
+      inactive: 'group flex items-center px-3 py-3 rounded-md menu-inactive',
+      active: 'group flex items-center px-3 py-3 rounded-md menu-active'
     }
   }
 
@@ -145,7 +145,7 @@ export default function Menu() {
             <ActiveLink href="/events" activeClassName={className.mobile.active}>
               <a className={className.mobile.inactive}>
                 {/* Heroicon name: lightning-bolt */}
-                <svg className="mr-3 h-6 w-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <svg className="mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />
                 </svg>
                 Events
@@ -155,7 +155,7 @@ export default function Menu() {
             <ActiveLink href="/tickets" activeClassName={className.mobile.active}>
               <a className={className.mobile.inactive}>
                 {/* Heroicon name: ticket */}
-                <svg className="mr-3 h-6 w-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <svg className="mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z" />
                 </svg>
                 My Tickets
@@ -165,7 +165,7 @@ export default function Menu() {
             <ActiveLink href="/calendar" activeClassName={className.mobile.active}>
               <a className={className.mobile.inactive}>
                 {/* Heroicon name: calendar */}
-                <svg className="mr-3 h-6 w-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <svg className="mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                 </svg>
                 Calendar
@@ -178,7 +178,7 @@ export default function Menu() {
               <ActiveLink href="/deadmau5/manage/settings" activeClassName={className.mobile.active}>
                 <a className={className.mobile.inactive}>
                   {/* Heroicon name: cog */}
-                  <svg className="mr-3 h-6 w-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <svg className="mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                   </svg>
@@ -189,7 +189,7 @@ export default function Menu() {
               <ActiveLink href="/help" activeClassName={className.mobile.active}>
                 <a className={className.mobile.inactive}>
                   {/* Heroicon name: question-mark-circle */}
-                  <svg className="mr-3 h-6 w-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <svg className="mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
                   Help
@@ -199,7 +199,7 @@ export default function Menu() {
               <ActiveLink href="/privacy" activeClassName={className.mobile.active}>
                 <a className={className.mobile.inactive}>
                   {/* Heroicon name: shield-check */}
-                  <svg className="mr-3 h-6 w-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <svg className="mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
                   </svg>
                   Privacy
