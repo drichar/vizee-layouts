213875b99983fb0fc7e38f825090c866b30280ce
Doug Richar <drichar@gmail.com>
2020-12-02 20:31:25 -0500

    Hero copy update

--- pages/index.js
+++ pages/index.js
@@ -95,13 +95,13 @@ export default function Home() {
             <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="sm:text-center lg:text-left">
                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-100 sm:text-5xl md:text-6xl">
-                  <span className="block xl:inline">Premium streaming</span>{' '}
-                  <span className="block text-pink-600 xl:inline">video network</span>
+                  <span className="block">Premium</span>{' '}
+                  <span className="block text-pink-600">video network</span>
                 </h1>
                 <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                  Sell videos. Set your own prices. <span className="text-gray-200 font-semibold">Earn up to 90% of every dollar.</span> No advertising, ever. 
+                  Sell videos. <span className="text-gray-200 font-semibold">Earn up to 90% of every dollar.</span>
                 </p>
-                <div className="mt-5 xs:mt-8 xs:flex xs:justify-center lg:justify-start">
+                <div className="mt-5 xs:mt-12 xs:flex xs:justify-center lg:justify-start">
                   <div className="rounded-md shadow">
                     <Link href="/">
                       <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-pink-600 hover:bg-pink-700 md:py-4 md:text-lg md:px-10">
