dc6abc1d7ad7f6851aadac006ee7ec1b7333077a
Doug Richar <drichar@gmail.com>
2020-12-05 02:06:46 -0500

    Add CSS vars for sizes

--- pages/deadmau5/event-id.js
+++ pages/deadmau5/event-id.js
@@ -3,8 +3,10 @@ import Layout from '../../components/Layout'
 export default function Event() {
   return (
     <Layout>
-      <div className="flex items-center justify-center" style={{ height: 'calc(100vh - 184px)'}}>
-        <h1 className="text-6xl xs:text-7xl sm:text-8xl md:text-9xl text-gray-900 font-bold">Event</h1>
+      <div className="flex items-center justify-center page-min-height">
+        <h1 className="text-6xl xs:text-7xl sm:text-8xl md:text-9xl text-gray-900 font-bold">
+          Event
+        </h1>
       </div>
     </Layout>
   )
