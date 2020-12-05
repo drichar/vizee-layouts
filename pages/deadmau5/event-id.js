538badaafa34e55392fc3a8b6e9a16c1c1db4d0e
Doug Richar <drichar@gmail.com>
2020-12-05 01:50:03 -0500

    Event page placeholder

---
+++ pages/deadmau5/event-id.js
@@ -0,0 +1,11 @@
+import Layout from '../../components/Layout'
+
+export default function Event() {
+  return (
+    <Layout>
+      <div className="flex items-center justify-center" style={{ height: 'calc(100vh - 184px)'}}>
+        <h1 className="text-6xl xs:text-7xl sm:text-8xl md:text-9xl text-gray-900 font-bold">Event</h1>
+      </div>
+    </Layout>
+  )
+}
