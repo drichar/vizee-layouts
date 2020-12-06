a5a722d2acce862bf0eaa76eec0a9b0bf91feb69
Doug Richar <drichar@gmail.com>
2020-12-05 20:46:51 -0500

    WIP

--- pages/deadmau5/event-id.js
+++ pages/deadmau5/event-id.js
@@ -3,11 +3,16 @@ import Layout from '../../components/Layout'
 export default function Event() {
   return (
     <Layout>
-      <div className="flex items-center justify-center page-min-height">
-        <h1 className="text-6xl xs:text-7xl sm:text-8xl md:text-9xl text-gray-900 font-bold">
-          Event
-        </h1>
-      </div>
+      <article className="max-w-screen-2xl mx-auto sm:p-6 lg:p-8 lg:flex page-min-height">
+        <section className="flex-grow">
+          <div className="aspect-w-16 aspect-h-9">
+            <iframe src="https://www.youtube.com/embed/tp4sRMoxlig" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </section>
+        <aside className="flex-none lg:w-80">
+
+        </aside>
+      </article>
     </Layout>
   )
 }
