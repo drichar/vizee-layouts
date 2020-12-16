c3c5af95510348e2e6bab2812756af28cf515348
Doug Richar <drichar@gmail.com>
2020-12-15 23:13:41 -0500

    Remove notifications button

--- components/Menu.js
+++ components/Menu.js
@@ -106,9 +106,8 @@ export default function Menu() {
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button className="bg-gray-850 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-850 focus:ring-white">
+            {/* <button className="bg-gray-850 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-850 focus:ring-white">
               <span className="sr-only">View notifications</span>
-              {/* Heroicon name: bell */}
               <svg
                 className="h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
@@ -124,7 +123,7 @@ export default function Menu() {
                   d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                 />
               </svg>
-            </button>
+            </button> */}
 
             <ProfileMenu />
           </div>
--- components/manage/Layout.js
+++ components/manage/Layout.js
@@ -60,9 +60,8 @@ export default function ManageLayout({ children }) {
               </Link>
             </div>
             <div className="ml-4 flex items-center md:ml-6">
-              <button className="bg-gray-900 p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-900 focus:ring-pink-600">
+              {/* <button className="bg-gray-900 p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-900 focus:ring-pink-600">
                 <span className="sr-only">View notifications</span>
-                {/* Heroicon name: bell */}
                 <svg
                   className="h-6 w-6"
                   xmlns="http://www.w3.org/2000/svg"
@@ -78,7 +77,7 @@ export default function ManageLayout({ children }) {
                     d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                   />
                 </svg>
-              </button>
+              </button> */}
 
               <ProfileMenu />
             </div>
